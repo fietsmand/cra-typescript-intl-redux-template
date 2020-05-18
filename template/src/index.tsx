cb561423b7a32bcfc8b59d8d4bcda455c03e8b1c
Daniel Huizenga <daniel.huizenga@bestseller.com>
2020-05-18 17:27:50 +0200

    Added Provider to index.tsx

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import ConnectedIntlProvider from './Providers/ConnectedIntlProvider';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './Redux/Store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ConnectedIntlProvider>
-      <App />
+    <ConnectedIntlProvider>      
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ConnectedIntlProvider>
   </React.StrictMode>,
   document.getElementById('root')
